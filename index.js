b76d607d225f085490ee2fa8e6026317b9ad73b4
nobo <505931977@qq.com>
2017-09-05 18:31:11 +0800

    Added      index.js Added      utils.js

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * 数据透视
+ * author: nobo.zhou
+ * email: nobo.zhou@foxmail.com
+ * version: 0.1
+ */
+ 
+var utils = require('./utils');
+
+/**
+ * @class
+ * @param {Object} data - 需要透视数据
+ * @param {Array} data.columns - 数据的列
+ * @param {Array} data.list - 数据列的值
+ * @param {Object} options - 透视参数
+ * @param {Array} options.pivotColumn - 透视列
+ * @param {Array} options.pivotRow - 透视行
+ * @param {Array} options.pivotValue - 透视值
+ */ 
+
+function PivotTable(data, options){
+	this._metedata = {}; 
+	  
+}
+
+PivotTable.prototype = {
+	constructor: PivotTable,
+	
+}
---
+++ utils.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var utils = {
+		
+};
+
+module.exports = utils;
