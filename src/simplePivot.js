a02f564b5e6d180f16e62eb60b85d9d777a56ce2
nobo <nobo.zhou@idreamsky.com>
2019-06-08 17:19:10 +0800

    v2.0

--- src/simplePivot.js
+++ src/simplePivot.js
@@ -1,23 +1,24 @@
-const warning = require('warning');
-const groupBy = require('nex-utils/groupBy');
-const difference = require('nex-utils/difference');
-const pick = require('nex-utils/pick');
+import warning from 'warning';
+import groupBy from 'lodash/groupBy';
+import difference from 'lodash/difference';
+import pick from 'lodash/pick';
+import zipObject from 'lodash/zipObject';
 
-function pickNmae(data = {}, keys = [], delimiter = '_'){
-	return keys.map( key => data[key] + '' ).join(delimiter);
+function pickName(data = {}, keys = [], delimiter = '_') {
+    return keys.map(key => data[key] + '').join(delimiter);
 }
 
-function makeMap(keys = []){
-	const maps = {};
-	keys.forEach( key => maps[key] = true );
-	return maps;
+function makeMap(keys = []) {
+    const maps = Object.create(null);
+    keys.forEach(key => maps[key] = true);
+    return maps;
 }
 
 /**
  * 数据透视转换
  * @param {array} data - 数据集
  * @param {array<string>} data.columns - 列头
- * @param {array<object>} data.list - 数据列表
+ * @param {array<array>} data.list - 数据列表
  * @param {object} options - 配置参数
  * @param {array<string>} options.rows - 数据透视行配置，此参数在simplePivot里无效
  * @param {array<string>} options.columns - 数据透视列配置
@@ -25,59 +26,82 @@ function makeMap(keys = []){
  * @param {string} [options.delimiter='_'] - 多级表头分隔符  
  */
 function simplePivot(data = {}, options = {}) {
-	const pOpts = {
-		columns: options.columns || [],
-		values: options.values || [],
-	};
-	const delimiter = options.delimiter || '_';
-	
-	data.columns = data.columns || [];
-	data.list = data.list || [];
-	
-	if( !pOpts.columns.length || !pOpts.values.length ) {
-		warning(
-			false,
-			'options.columns or options.values cannot be empty!'
-		);
-	}
-	
-	pOpts.rows = difference(data.columns, [].concat(pOpts.columns, pOpts.values));
-	
-	const dataset = data.list;
-	const groupData = groupBy(dataset, row => {
-		return '__' + pickNmae(row, pOpts.rows, delimiter);
-	});
-	
-	const newDataSet = [];
-	const newColumns = pOpts.rows.slice();
-	const newColumnsMap = makeMap(newColumns);
-	
-	for(let group in groupData) {
-		const gData = groupData[group];
-		let newData;
-		gData.forEach( (item, i) => {
-			if(!i) {
-				newData = pick(item, pOpts.rows);		
-			}
-			pOpts.values.forEach( vKey => {
-				const preKey = pickNmae(item, pOpts.columns, delimiter);
-				const columnName = preKey + delimiter + vKey;
-				newData[columnName] = item[vKey];
-				
-				if( !newColumnsMap[columnName] ) {
-					newColumnsMap[columnName] = true;
-					newColumns.push(columnName);	
-				}
-			} );
-		} );
-		
-		newDataSet.push( newData );
-	}
-	
-	return {
-		columns: newColumns,
-		list: newDataSet
-	}
+    const pOpts = {
+        columns: options.columns || [],
+        values: options.values || [],
+    };
+    const delimiter = options.delimiter || '_';
+
+    data.columns = data.columns || [];
+    data.list = data.list || [];
+
+    if (!pOpts.columns.length || !pOpts.values.length) {
+        warning(
+            false,
+            'options.columns or options.values cannot be empty!'
+        );
+    }
+
+    const undefColumns = difference([].concat(pOpts.columns, pOpts.values), data.columns);
+    if (undefColumns.length) {
+        warning(
+            false,
+            'simplePivot unknow columns: ' + undefColumns.join(',')
+        );
+    }
+
+    pOpts.rows = difference(data.columns, [].concat(pOpts.columns, pOpts.values));
+
+    const dataset = data.list.map(d => zipObject(data.columns, d));
+
+    const groupNames = []; //记录分组顺序
+    const groupNameMaps = Object.create(null);
+    const groupData = groupBy(dataset, row => {
+        const gName = pickName(row, pOpts.rows, delimiter);
+
+        if (!(gName in groupNameMaps)) {
+            groupNameMaps[gName] = true;
+            groupNames.push(gName);
+        }
+
+        return gName;
+    });
+
+    const newDataSet = [];
+    const newColumns = pOpts.rows.slice();
+    const newColumnsMap = makeMap(newColumns);
+
+    for (let i = 0; i < groupNames.length; i++) {
+        const gName = groupNames[i];
+        const gData = groupData[gName];
+        let newData;
+        gData.forEach((item, i) => {
+            if (!i) {
+                newData = pick(item, pOpts.rows);
+            }
+            pOpts.values.forEach(vKey => {
+                const preKey = pickName(item, pOpts.columns, delimiter);
+                const columnName = preKey + delimiter + vKey;
+                newData[columnName] = item[vKey];
+
+                if (!newColumnsMap[columnName]) {
+                    newColumnsMap[columnName] = true;
+                    newColumns.push(columnName);
+                }
+            });
+        });
+
+        newDataSet.push(newData);
+    }
+
+    const list = newDataSet.map(row => {
+        return newColumns.map(column => row[column]);
+    });
+
+    return {
+        columns: newColumns,
+        list,
+    }
 }
 
-module.exports = simplePivot;
+export default simplePivot;
--- tests/noRows.js
+++ tests/noRows.js
@@ -1,24 +1,41 @@
-const simplePivot = require('../lib/simplePivot');
+const simplePivot = require('../index');
 
 const dataset = {
-	columns: ['性别', '人数'],
-	list: [
-		{"性别": "男", "人数": 1584},
-		{"性别": "女", "人数": 1514},
-	]	
+    columns: ['性别', '人数'],
+    list: [
+        ['男', '1584'],
+        ['女', '1514'],
+    ]
 };
 
-//dataset.columns = _.difference( dataset.columns, ['道具id'] );
+const pivotData = simplePivot(dataset, {
+    columns: ['性别'],
+    values: ['人数']
+});
 
-//dataset.list = dataset.list.map( d => _.omit( d, ['道具id'] ) );
+console.log(pivotData);
 
-const pivotData = simplePivot( dataset, {
-	//columns: ['道具类型', '道具名称'],
-	columns: ['性别'],
-	values: ['人数']	
-} );
+const dataset1 = {
+    columns: ['日期', '类型', '数值'],
+    list: [
+        ['20170809', 'A', 1],
+        ['20170809', 'B', 2],
+        ['20170808', 'A', 3],
+        ['20170808', 'B', 4],
+        ['20170808', 'C', 5],
+        ['20170807', 'A', 6],
+        ['20170807', 'B', 7],
+    ]
+};
 
-console.log( pivotData );
+console.log('====================')
+
+const pivotData1 = simplePivot(dataset1, {
+    columns: ['类型', 'ABC'],
+    values: ['数值', 'ccc']
+});
+
+console.log(pivotData1);
 
 
 
