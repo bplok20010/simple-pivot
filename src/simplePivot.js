bbe2f974eba0d4024e04554965ed1f38c4ff3a53
bplok20010 <505931977@qq.com>
2019-08-13 18:22:20 +0800

    1. 新增数值占位符参数: valueMacro

--- src/simplePivot.js
+++ src/simplePivot.js
@@ -1,16 +1,22 @@
-import warning from 'warning';
-import groupBy from 'lodash/groupBy';
-import difference from 'lodash/difference';
-import pick from 'lodash/pick';
-import zipObject from 'lodash/zipObject';
+import warning from "warning";
+import groupBy from "lodash/groupBy";
+import difference from "lodash/difference";
+import pick from "lodash/pick";
+import zipObject from "lodash/zipObject";
 
-function pickName(data = {}, keys = [], delimiter = '_') {
-    return keys.map(key => data[key] + '').join(delimiter);
+function pickName(data = {}, keys = [], delimiter = "_") {
+    return keys.map(key => data[key] + "").join(delimiter);
+}
+
+function pickName2(data = {}, keys = [], vKey, delimiter = "_", VALUE_MACRO) {
+    return keys
+        .map(key => (key === VALUE_MACRO ? vKey : data[key] + ""))
+        .join(delimiter);
 }
 
 function makeMap(keys = []) {
     const maps = Object.create(null);
-    keys.forEach(key => maps[key] = true);
+    keys.forEach(key => (maps[key] = true));
     return maps;
 }
 
@@ -23,34 +29,45 @@ function makeMap(keys = []) {
  * @param {array<string>} options.rows - 数据透视行配置，此参数在simplePivot里无效
  * @param {array<string>} options.columns - 数据透视列配置
  * @param {array<string>} options.values - 数据透视值配置
- * @param {string} [options.delimiter='_'] - 多级表头分隔符  
+ * @param {array<string>} options.valueMacro - 数据透视表中的数值占位符
+ * @param {string} [options.delimiter='_'] - 多级表头分隔符
  */
 function simplePivot(data = {}, options = {}) {
     const pOpts = {
         columns: options.columns || [],
-        values: options.values || [],
+        values: options.values || []
     };
-    const delimiter = options.delimiter || '_';
+    const delimiter = options.delimiter || "_";
+    const VALUE_MACRO = options.valueMacro || "E_VALUE";
+
+    if (pOpts.columns.indexOf(VALUE_MACRO) === -1) {
+        pOpts.columns = [].concat(pOpts.columns, VALUE_MACRO);
+    }
+
+    const columnsWithoutMacro = pOpts.columns.filter(
+        column => column !== VALUE_MACRO
+    );
 
     data.columns = data.columns || [];
     data.list = data.list || [];
 
-    if (!pOpts.columns.length || !pOpts.values.length) {
-        warning(
-            false,
-            'options.columns or options.values cannot be empty!'
-        );
+    if (!pOpts.values.length) {
+        //!pOpts.columns.length ||
+        warning(false, "options.values cannot be empty!"); //options.columns or
     }
 
-    const undefColumns = difference([].concat(pOpts.columns, pOpts.values), data.columns);
+    const undefColumns = difference(
+        [].concat(columnsWithoutMacro, pOpts.values),
+        data.columns
+    );
     if (undefColumns.length) {
-        warning(
-            false,
-            'simplePivot unknow columns: ' + undefColumns.join(',')
-        );
+        warning(false, "simplePivot unknow columns: " + undefColumns.join(","));
     }
 
-    pOpts.rows = difference(data.columns, [].concat(pOpts.columns, pOpts.values));
+    pOpts.rows = difference(
+        data.columns,
+        [].concat(columnsWithoutMacro, pOpts.values)
+    );
 
     const dataset = data.list.map(d => zipObject(data.columns, d));
 
@@ -80,8 +97,13 @@ function simplePivot(data = {}, options = {}) {
                 newData = pick(item, pOpts.rows);
             }
             pOpts.values.forEach(vKey => {
-                const preKey = pickName(item, pOpts.columns, delimiter);
-                const columnName = preKey + delimiter + vKey;
+                const columnName = pickName2(
+                    item,
+                    pOpts.columns,
+                    vKey,
+                    delimiter,
+                    VALUE_MACRO
+                );
                 newData[columnName] = item[vKey];
 
                 if (!newColumnsMap[columnName]) {
@@ -100,8 +122,8 @@ function simplePivot(data = {}, options = {}) {
 
     return {
         columns: newColumns,
-        list,
-    }
+        list
+    };
 }
 
 export default simplePivot;
--- tests/simple.js
+++ tests/simple.js
@@ -1,16 +1,19 @@
-const simplePivot = require('../index');
-// const zipObject = require('nex-utils/zipObject');
-const dataset = require('./dataset.json').data;
+const simplePivot = require("../index");
+const dataset = require("./dataset.json").data;
 
-// dataset.list = dataset.list.map(d => zipObject(dataset.columns, d));
+{
+    const pivotData = simplePivot(dataset, {
+        columns: ["日期"],
+        values: ["数量", "用户数"]
+    });
 
-const pivotData = simplePivot(dataset, {
-    //columns: ['道具类型', '道具名称'],
-    columns: ['日期'],
-    values: ['数量', '用户数']
-});
+    console.log(pivotData);
+}
 
-console.log(pivotData);
-
-
-
+{
+    const pivotData = simplePivot(dataset, {
+        columns: ["E_VALUE", "道具类型", "道具名称"],
+        values: ["数量", "用户数"]
+    });
+    console.log(pivotData);
+}
